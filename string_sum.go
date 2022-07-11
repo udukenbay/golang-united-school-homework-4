3d13abb1905f7f8c2f2ddae0ede388949105ce37
Дукенбай Урзия Әбдіхалыққызы <Urziya.Dukenbay@kaspi.kz>
2022-07-11 13:57:27 +0600

    error in golang

--- string_sum.go
+++ string_sum.go
@@ -1,15 +1,22 @@
-package string_sum
+//package string_sum
+package main
 
 import (
-	"errors"
+	"fmt"
+	"strconv"
+	"strings"
 )
+
+func main() {
+	fmt.Println(StringSum(" 3+5 "))
+}
 
 //use these errors as appropriate, wrapping them with fmt.Errorf function
 var (
 	// Use when the input is empty, and input is considered empty if the string contains only whitespace
-	errorEmptyInput = errors.New("input is empty")
+	errorEmptyInput = fmt.Errorf("input is empty")
 	// Use when the expression has number of operands not equal to two
-	errorNotTwoOperands = errors.New("expecting two operands, but received more or less")
+	errorNotTwoOperands = fmt.Errorf("expecting two operands, but received more or less")
 )
 
 // Implement a function that computes the sum of two int numbers written as a string
@@ -19,9 +26,48 @@ var (
 //For the cases, when the input expression is not valid(contains characters, that are not numbers, +, - or whitespace)
 // the function should return an empty string and an appropriate error from strconv package wrapped into your own error
 // with fmt.Errorf function
-//
-// Use the errors defined above as described, again wrapping into fmt.Errorf
 
 func StringSum(input string) (output string, err error) {
-	return "", nil
+	input = strings.TrimSpace(input)
+
+	if len(input) == 0 {
+		return "", errorEmptyInput
+	}
+
+	operand := []string{}
+	s := []int64{}
+
+	for _, k := range input {
+		if k == '+' || k == '-' {
+			operand = append(operand, string(k))
+		} else {
+			d, err := strconv.ParseInt(string(k), 10, 64)
+			if err != nil {
+				return "", errorNotTwoOperands
+			} else {
+				s = append(s, d)
+			}
+		}
+	}
+
+	cntr := 0
+	for _, t := range operand {
+		if t == "-" {
+			cntr++
+		}
+	}
+
+	var sum int64
+
+	if cntr%2 == 0 {
+		for _, i := range s {
+			sum += i
+		}
+	} else {
+		for _, i := range s {
+			sum -= i
+		}
+	}
+
+	return string(sum), nil
 }
